14ad6886e4af31aeb0e4a5bfa87635eb5001cace
hasan <hasanbatis440@gmail.com>
2021-05-03 10:56:48 +0300

    many features has been developed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user , :logged_in?
+  #to make available on  views and controller
+  def current_user
+   @current_user ||= User.find(session[:user_id]) if session[:user_id]
+ #if current_user exist or equil to this user then keep going
+    end
+
+    def logged_in?
+      !!current_user
+      #to check if current_user is access, yes or no
+    end
+
+    def require_user
+      if !logged_in?
+        flash[:error] = "You must be logged in to perform that action"
+        redirect_to login_path
+    end
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,30 @@
 class SessionsController < ApplicationController
+  before_action :logged_in_redirect, only: [:new, :create]
+  def new
+  end
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+    session[:user_id] = user.id
+    flash[:success] = "Welcome Back"
+    redirect_to root_path
+    else
+      flash.now[:error]= "Enter a valid username and password"
+      render 'login'
+    end
+  end
+  def destroy
+    session[:user_id]  = nil
+    flash[:success] = "You logged out"
+    redirect_to login_path
+  end
 
+  private
 
+  def logged_in_redirect
+    if logged_in?
+      flash[:erorr] = "You are already lodded in"
+      redirect_to root_path
+    end
+  end
 end
