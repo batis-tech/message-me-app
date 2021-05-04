8b7a54c5e5fe8c24a4a5be7b7edb80bab019abc3
hasan <hasanbatis440@gmail.com>
2021-05-05 00:07:24 +0300

    finalizing the web application

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,6 +16,19 @@
 //= require_tree .
 //= require jquery
 //= require semantic-ui
+scroll_bottom = function() {
+ if ($('#messages').length > 0) {
+   $('#messages').scrollTop($('#messages')[0].scrollHeight);
+ }
+}
+submit_message = function() {
+  $('#message_body').on('keydown', function(e){
+    if (e.keyCode == 13) {
+      $('buttom').click();
+      e.target.value = ""
+    };
+  });
+};
 $(document).on('turbolinks:load', function () {
   $('.ui.dropdown')
     .dropdown()
@@ -28,4 +41,6 @@ $(document).on('turbolinks:load', function () {
       ;
     })
   ;
+  submit_message();
+  scroll_bottom();
 })
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,6 @@
 class Message < ApplicationRecord
   belongs_to :user
   validates :body , presence: true
+  #to get the las created 20 massages
+  scope :custom_display, -> { order(:created_at).last(20) }
 end
